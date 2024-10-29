e595418bf720369eacf56a387e45298316bc042f
vladilemus <vladimirlemus404@outlook.com>
2024-10-28 23:18:06 -0600

    actualizacion del 28 de octubre del 2024

---
+++ resources/views/navbar/navbar.blade.php
@@ -0,0 +1,96 @@
+<!--
+    @author Vladimir Lemus
+    Codice
+    Nombre del Código: navbar.blade.php
+    Fecha de Creación: 28/10/2024 revisado por Angel Geovanni Marcial Morales
+
+    Modificaciones:
+
+    Descripción: Este archivo PHP contiene la cabecera de la interfaz administrador.
+-->
+<?php
+
+$tipo = session('rol');
+
+?>
+<?php 
+
+switch ($tipo){
+    case 'administrador':
+        ?>
+            <div class="container mt-5">
+                <nav class="navbar custom-bg  fixed-top "> <!--navbar-expand-lg        -->
+                <div class="container-fluid "> 
+            
+                    <img src="{{ asset('logo.png') }}" alt="Logo" class="img-fluid" style="height: 50px;">
+            
+                    <h2 class="cabecera mx-auto" >Administración</h2>
+            
+                    <form action="{{ url('cerrar-session') }}" method="POST" style="display: inline;">
+                        @csrf
+                        <button class="navbar-toggler btn-cabecera" type="submit" aria-label="Cerrar sesión">
+                            <h3><</h3>
+                        </button>
+                    </form>
+                    
+                </div>
+                </nav>
+            </div>
+        <?php
+        break;
+    case 'alumno':
+        ?>
+            <div class="container mt-5">
+                <nav class="navbar custom-bg  fixed-top "> <!--navbar-expand-lg        -->
+                <div class="container-fluid "> 
+            
+                    <img src="{{ asset('logo.png') }}" alt="Logo" class="img-fluid" style="height: 50px;">
+            
+                    <h2 class="cabecera mx-auto" >Portal Alumno</h2>
+            
+                    <form action="{{ url('cerrar-session') }}" method="POST" style="display: inline;">
+                        @csrf
+                        <button class="navbar-toggler btn-cabecera" type="submit" aria-label="Cerrar sesión">
+                            <h3><</h3>
+                        </button>
+                    </form>
+                    
+                </div>
+                </nav>
+            </div>
+            
+        <?php
+        break;
+    
+    case 'maestro':
+        ?>
+            <div class="container mt-5">
+                <nav class="navbar custom-bg  fixed-top "> <!--navbar-expand-lg        -->
+                <div class="container-fluid "> 
+            
+                    <img src="{{ asset('logo.png') }}" alt="Logo" class="img-fluid" style="height: 50px;">
+            
+                    <h2 class="cabecera mx-auto" >Portal Maestros</h2>
+            
+                    <form action="{{ url('cerrar-session') }}" method="POST" style="display: inline;">
+                        @csrf
+                        <button class="navbar-toggler btn-cabecera" type="submit" aria-label="Cerrar sesión">
+                            <h3><</h3>
+                        </button>
+                    </form>
+                    
+                </div>
+                </nav>
+            </div>
+        <?php
+        break;
+
+    default:
+        // Código a ejecutar si $variable no coincide con ninguno de los casos anteriores
+        echo "Valor no encontrado";
+        break;
+}
+
+?>
+
+
