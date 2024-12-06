52c7517628474fdad406b623f1d8a5728c39cf6d
AleCT01 <111063020+Dark227@users.noreply.github.com>
2024-12-05 23:14:19 -0600

    Actualizar Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -6,7 +6,7 @@ Fecha de Creación: 01/11/2024 revisado por Angel Geovanni Marcial Morales
 
 Modificaciones:
 
-Descripción: Este archivo PHP es una parte esencial del framework Laravel y se encuentra en el contexto del proyecto de control de accesos a la UAPT. Su función principal es manejar la programación de comandos y tareas en la aplicación mediante el uso del componente de programación de tareas de Laravel.
+Descripción: Este archivo PHP es una parte esencial del framework de Laravel y se encuentra en el contexto del proyecto de control de accesos a la UAPT. Su función principal es manejar la programación de comandos y tareas en la aplicación mediante el uso del componente de programación de tareas de Laravel.
 
 */
 namespace App\Console;
